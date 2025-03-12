d84a2361d65b7ca268f30414eadc57a71eff3928
hasan <hasanpekedis365@hotmail.com>
2025-03-12 17:59:00 +0300

    new classes and fixes

---
+++ src/classes/generate_lip_sync.py
@@ -0,0 +1,20 @@
+import os
+import subprocess
+
+def lip_sync(audio_path, output_path):
+    """Call Wav2Lip to generate a talking avatar"""
+    
+    wav2lip_path = "src/Wav2Lip"  # Change this to the actual Wav2Lip directory
+    os.chdir(wav2lip_path)  # Change directory to Wav2Lip
+    
+    # Run the inference script
+    subprocess.run([
+        "python3", "inference.py",
+        "--checkpoint_path", "checkpoints/wav2lip_gan.pth",
+        "--face", "../../assets/images/model.jpg",
+        "--audio", audio_path,
+        "--outfile", output_path
+    ])
+
+    print(f"Generated lip-synced video: {output_path}")
+
---
+++ src/classes/generator.py
@@ -0,0 +1,30 @@
+from classes.generate_sound import GenerateSound
+from classes.generate_lip_sync import lip_sync
+import os
+class Generator:
+    def __init__(self, guid, text):
+        self.guid = guid
+        self.text = text
+       
+    def start(self):
+
+        sound_path = f"assets/sounds/{self.guid}.wav"
+        video_path = f"assets/videos/{self.guid}.mp4"
+
+
+        generate_sound = GenerateSound("leafy-container-453009-a1-a2f7e8069251.json")
+        
+        os.makedirs(os.path.dirname(sound_path), exist_ok=True)
+
+        generate_sound.synthesize(self.text, sound_path)
+
+        
+        lip_sync("../../" + sound_path,"../../" + video_path)
+
+
+
+
+
+
+
+
